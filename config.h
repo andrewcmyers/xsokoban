b704f28d6c7df44480a719b7273f4d8c6f5fa810
jik <jik>
1992-07-08 22:55:47 +0000

    Only define symbols if they aren't already defined.

--- config.h
+++ config.h
@@ -4,28 +4,38 @@
 /**/
 /* SCREENPATH: the name of the directioy where the screen file are held */
 /**/
+#ifndef SCREENPATH
 #define SCREENPATH "/usr/tmp/xsokoban.v2/screens"
+#endif
 
 /**/
 /* SAVEPATH: the name of the path where save files are held */
 /*           Attention: Be sure that there are no other files with */
 /*                      the name <username>.sav                    */
 /**/
+#ifndef SAVEPATH
 #define SAVEPATH "/usr/tmp/xsokoban.v2/saves"
+#endif
 
 /* BITPATH: the full pathname to the bitmap file defaults. */
+#ifndef BITPATH
 #define BITPATH "/usr/tmp/xsokoban.v2/bitmaps/defaults"
+#endif
 
 /**/
 /* LOCKPATH: temporary file which is created to ensure that no users */
 /*           work with the scorefile at the same time                */
 /**/
+#ifndef LOCKFILE
 #define LOCKFILE "/tmp/score.slock"
+#endif
 
 /**/
 /* SCOREFILE: the full pathname of the score file */
 /**/
+#ifndef SCOREFILE
 #define SCOREFILE "/usr/tmp/xsokoban.v2/sokoban.slock"
+#endif
 
 /**/
 /* MAXUSERNAME: defines the maximum length of a system's user name */
@@ -40,9 +50,13 @@
 /**/
 /* SUPERUSER: defines the name of the game superuser */
 /**/
+#ifndef SUPERUSER
 #define SUPERUSER "jt1o"
+#endif
 
 /**/
 /* PASSWORD: defines the password necessary for creating a new score file */
 /**/
+#ifndef PASSWORD
 #define PASSWORD "Greezooble"
+#endif
